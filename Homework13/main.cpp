75c812869c857dd6884e6a169eefc7b3e5c97942
Aniket Sedhai <asedhai@masonite.com>
2024-07-15 00:17:00 -0500

    Initial Commit

---
+++ Homework13/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework13
+//
+//  Created by Aniket Sedhai on 7/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
